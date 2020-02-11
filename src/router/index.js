4a40781d56880c6e8499e7f896b1c43217d954e7
PlusZ <5655619+plusz@user.noreply.gitee.com>
2020-02-11 19:19:57 +0800

    完成分类参数

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ import Welcome from "../components/Welcome";
 import Users from "../components/user/Users";
 import Rights from '../components/power/Rights';
 import Roles from '../components/power/Roles';
-import Cate from '../components/goods/Cate'
+import Cate from '../components/goods/Cate';
+import Params from '../components/goods/Params'
 
 Vue.use(VueRouter);
 
@@ -41,6 +42,10 @@ const routes = [{
       {
         path: "/categories",
         component: Cate
+      },
+      {
+        path: "/params",
+        component: Params
       },
     ]
   }
