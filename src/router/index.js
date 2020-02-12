d30e82cb5e293dd14bbfcdcc61be4c9a64df5079
PlusZ <5655619+plusz@user.noreply.gitee.com>
2020-02-12 18:24:12 +0800

    完成报表

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,8 @@ import Cate from '../components/goods/Cate';
 import Params from '../components/goods/Params';
 import GoodsList from '../components/goods/List';
 import Add from '../components/goods/Add.vue';
-import Order from '../components/order/Order.vue'
+import Order from '../components/order/Order.vue';
+import Report from '../components/report/Report.vue';
 
 Vue.use(VueRouter);
 
@@ -61,6 +62,10 @@ const routes = [{
       {
         path: "/orders",
         component: Order
+      },
+      {
+        path: "/Report",
+        component: Report
       },
     ]
   }
